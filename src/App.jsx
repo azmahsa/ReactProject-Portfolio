f5ef1ce5120eae4bb41e1dd7f7d3dd382380b1f1
mahsaalizolfi <mahsaalizolfii2@gmail.com>
2023-10-30 19:32:35 -0700

    update Work.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
-import Work from "./components/work";
+import Work from "./components/Work"
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 function App() {
