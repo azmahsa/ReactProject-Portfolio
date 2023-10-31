401df86be1d73622681b90aef3be86a59acf2d6d
mahsaalizolfi <mahsaalizolfii2@gmail.com>
2023-10-30 18:08:50 -0700

    Update All

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
-import Sidenav from './components/Sidenav'
-import Main from './components/Main'
-import Work from './components/work'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import { useState } from "react";
+import Sidenav from "./components/Sidenav";
+import Main from "./components/Main";
+import Work from "./components/work";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 function App() {
-
   return (
-    
-     <div>
-      <Sidenav/>
-      <Main/>
-      <Work/>
-      <Projects/>
-      <Contact/>
-     </div>
-    
-  )
+    <div>
+      <Sidenav />
+      <Main />
+      <Work />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,51 +1,54 @@
 import React, { useState } from "react";
 import WorkItem from "./WorkItem";
 
-
 const Work = () => {
-  const [data,setData]= useState([
+  const [data, setData] = useState([
     {
-        year:2020,
-        title : 'Content Creator',
-        duration : '3 Years',
-        details : 
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur dolorem, numquam repellat tempora maxime deserunt quidem eligendi atque quasi unde quaerat voluptatum, esse doloribus distinctio et debitis delectus eum."
+      year: 2020,
+      title: "Content Creator",
+      duration: "3 Years",
+      details:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur dolorem, numquam repellat tempora maxime deserunt quidem eligendi atque quasi unde quaerat voluptatum, esse doloribus distinctio et debitis delectus eum.",
     },
     {
-        year:2017,
-        title : 'Google',
-        duration : '3 Years',
-        details : 
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur dolorem, numquam repellat tempora maxime deserunt quidem eligendi atque quasi unde quaerat voluptatum, esse doloribus distinctio et debitis delectus eum."
+      year: 2017,
+      title: "Google",
+      duration: "3 Years",
+      details:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur dolorem, numquam repellat tempora maxime deserunt quidem eligendi atque quasi unde quaerat voluptatum, esse doloribus distinctio et debitis delectus eum.",
     },
     {
-        year:2015,
-        title : 'Amazon',
-        duration : '3 Years',
-        details : 
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur dolorem, numquam repellat tempora maxime deserunt quidem eligendi atque quasi unde quaerat voluptatum, esse doloribus distinctio et debitis delectus eum."
+      year: 2015,
+      title: "Amazon",
+      duration: "3 Years",
+      details:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur dolorem, numquam repellat tempora maxime deserunt quidem eligendi atque quasi unde quaerat voluptatum, esse doloribus distinctio et debitis delectus eum.",
     },
     {
-        year:2012,
-        title : 'Facebook',
-        duration : '3 Years',
-        details : 
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur dolorem, numquam repellat tempora maxime deserunt quidem eligendi atque quasi unde quaerat voluptatum, esse doloribus distinctio et debitis delectus eum."
-    }
-  ])
-    return ( 
-     <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-        <h1 className="text-4xl font-bold text-center text-[#001b5e]">Work</h1>
-     
-        <>
-        {data.map((item,index)=>(
-              <WorkItem key={index} title={item.title} year={item.year} duration={item.duration} details={item.details} />
-          
+      year: 2012,
+      title: "Facebook",
+      duration: "3 Years",
+      details:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur dolorem, numquam repellat tempora maxime deserunt quidem eligendi atque quasi unde quaerat voluptatum, esse doloribus distinctio et debitis delectus eum.",
+    },
+  ]);
+  return (
+    <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Work</h1>
+
+      <>
+        {data.map((item, index) => (
+          <WorkItem
+            key={index}
+            title={item.title}
+            year={item.year}
+            duration={item.duration}
+            details={item.details}
+          />
         ))}
-        </>
-     </div>
-     
-     );
-}
- 
+      </>
+    </div>
+  );
+};
+
 export default Work;
