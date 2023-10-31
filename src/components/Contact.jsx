e32626fcbf6d841bff150e265cbe545117d5a445
mahsaalizolfi <mahsaalizolfii2@gmail.com>
2023-10-30 18:16:54 -0700

    update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,10 @@
 const Contact = () => {
   return (
-    <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+    <div id="contact" className="max-w-[800px] m-auto md:pl-20 p-4 py-16">
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
         Contact
       </h1>
-      <form
+      <form 
         action="https://getform.io/f/6774076c-6f39-4263-b59f-05828c1d7979"
         method="POST"
         encType="multipart/form-data"
